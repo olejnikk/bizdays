f1c5a4e7600457e93fb1322990a7499fb25a455c
Denis Shklyaev <sdv>
2022-11-18 15:24:51 +0300

    break-time support

--- src/main/java/com/kolejnik/bizdays/schedule/BusinessDay.java
+++ src/main/java/com/kolejnik/bizdays/schedule/BusinessDay.java
@@ -6,6 +6,8 @@ import java.time.LocalTime;
 public class BusinessDay {
 
     private final LocalTime startTime;
+    private final LocalTime breakStartTime;
+    private final LocalTime breakEndTime;
     private final LocalTime endTime;
 
     public boolean contains(LocalTime time) {
@@ -19,11 +21,15 @@ public class BusinessDay {
     public BusinessDay(int startHour, int startMinute, int endHour, int endMinute) {
         this.startTime = LocalTime.of(startHour, startMinute);
         this.endTime = LocalTime.of(endHour, endMinute);
+        this.breakStartTime = null;
+        this.breakEndTime = null;
     }
 
-    public BusinessDay(LocalTime startTime, LocalTime endTime) {
+    public BusinessDay(LocalTime startTime, LocalTime endTime, LocalTime breakStartTime, LocalTime breakEndTime) {
         this.startTime = startTime;
         this.endTime = endTime;
+        this.breakStartTime = breakStartTime;
+        this.breakEndTime = breakEndTime;
     }
 
     public LocalTime getStartTime() {
@@ -34,4 +40,11 @@ public class BusinessDay {
         return endTime;
     }
 
+    public LocalTime getBreakStartTime() {
+        return breakStartTime;
+    }
+
+    public LocalTime getBreakEndTime() {
+        return breakEndTime;
+    }
 }
